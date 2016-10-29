c04c396473295983fd7b06c6d1b1c9329b01383f
Caleb Kester <calebkester@gmail.com>
2016-10-29 11:05:40 -0500

    configuring queue

--- src/components/Drill-Quiz.vue
+++ src/components/Drill-Quiz.vue
@@ -9,17 +9,21 @@
     </div>
 
     <div v-if="question" class="content">
-      <h2>{{ question.title }} #{{ attempt }}</h2>
+      <h2>{{ question.title }} #{{ user.attempt }}</h2>
       <div v-html="question.question"></div>
-			<input type="text" :placeholder="placeholder" />
-			<button type="button">Score</button>
+			<form v-on:submit.prevent="submitScore">
+				<input v-model.number="user.answer" :placeholder="placeholder" type="number" step="any" required />
+				<button type="submit">Score</button>
+			</form>
 
-			<div>
-				{{question.seed}}
-				{{question.answer}}
-				{{question.tolerance}}
+			<div v-if="user.isCorrect != null">
+				<h3>{{user.isCorrect}}!</h3>
+				<strong>Solution:</strong>
+				<div v-html="question.solution"></div>
 
 			</div>
+			<button type="button" v-on:click="queueData()">Queue</button>
+			<button type="button" v-on:click="nextQuestion()">Next</button>
     </div>
 
 
@@ -37,8 +41,14 @@
 			return {
 				loading: false,
 				error: null,
-				attempt: 1,
-				question: null
+				questionID: this.$route.params.id,
+				question: null,
+				questionQueue: [],
+				user: {
+					attempt: 1,
+					isCorrect: null,
+					answer: null
+				}
 			}
 		},
 		computed: {
@@ -58,6 +68,65 @@
 			}
 		},
 		methods: {
+			// Scores the attempt
+			submitScore: function () {
+				var tolerance = this.question.tolerance * this.question.answer
+				this.user.isCorrect = (
+					this.user.answer >= (this.question.answer - tolerance) &&
+					this.user.answer <= (this.question.answer + tolerance)
+				)
+				this.uploadScore()
+			},
+
+			// Upload's attempt to the db
+			uploadScore: function () {
+				var postObj = {
+					questionID: this.question.id,
+					seed: this.question.seed,
+					answer: this.question.answer,
+					tolerance: this.question.tolerance,
+					user: this.user
+				}
+				this.$http.post('http://localhost:3004/attempts/', postObj).then((response) => {
+					// It worked!
+					console.log('Post Success!')
+					console.log(response)
+				}, (response) => {
+					// It failed :( that's okay, we didn't need that data anyway
+					console.log('Post of Data Failed :( ')
+					console.log(response)
+				})
+			},
+
+			// Queues the next question
+			queueData: function () {
+				// Check to make sure this request won't get cached
+				this.questionID = (this.questionID === 1) ? 2 : 1
+				this.$http.get('http://localhost:3004/questions/' + this.questionID).then((response) => {
+					this.questionQueue.push(response.data)
+					console.log(this.questionQueue)
+				}, (response) => {
+					// add failure
+				})
+			},
+
+			// Grabs a new question from our queue and then pulls more
+			nextQuestion: function () {
+				if (this.questionQueue.length === 0) {
+					this.queueData()
+				}
+				console.log(this.questionQueue.length)
+				// while (this.questionQueue.length < 2) {
+				// 	console.log(this.questionQueue.length)
+				//	this.queueData()
+				// }
+				// Probably should make this more foolproof, if the api doesn't
+				// return anything then what?
+				this.queueData()
+				this.question = this.questionQueue.shift()
+			},
+
+			// Grabs the initial data
 			fetchData: function () {
 				this.error = this.question = null
 				this.loading = true
